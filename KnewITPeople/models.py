3a433c287d1631857d77bf9245872d242ad77a39
Arleking <Abdukarim600@gmail.com>
2023-12-11 19:50:32 +0600

    Update models for correct work of admin panel

--- KnewITPeople/models.py
+++ KnewITPeople/models.py
@@ -15,24 +15,34 @@ class Person(models.Model):
     work_ex = models.TextField(default=None)
     certificate_knewit = models.BooleanField(default=False)
     Resume = models.FileField(upload_to='resume/', default=None)
+    def __str__(self) -> str:
+        return f'{self.name}'
 
 class Claim_work(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE, default=None)
     stack = models.CharField(max_length=128)
     frame = models.CharField(max_length=128)
     desired_salary = models.IntegerField(default=0)
+    def __str__(self) -> str:
+        return f'{self.person.name}'
 
 class Tech(models.Model):
-    frame = models.CharField(max_length=128)
+    frame = models.TextField(default=None)
     stack = models.CharField(max_length=128)
-
+    def __str__(self) -> str:
+        return f'{self.stack}'
+    
 class soc_links(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     instagram = models.TextField(default=None)
     WPP = models.TextField(default=None)
     Github = models.TextField(default=None)
     twitter = models.TextField(default=None)
+    def __str__(self) -> str:
+        return f'{self.person.name}'
 
 class certificates(models.Model):
     file = models.FileField(upload_to='certificates/')
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return f'{self.person.name}'
