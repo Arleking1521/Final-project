dd3c582e44f4a77f04f0c265b29ba28aa378c29f
Arleking <Abdukarim600@gmail.com>
2023-12-11 17:43:26 +0600

    Update models

--- KnewITPeople/models.py
+++ KnewITPeople/models.py
@@ -5,22 +5,22 @@ from django.utils import timezone
 
 class Person(models.Model):
     name = models.CharField(max_length=128)
-    age = models.IntegerField
-    phone = models.TextField
-    email = models.TextField
-    photo = models.ImageField
-    living_place = models.TextField
-    skills = models.TextField
-    languages= models.TextField
-    work_ex = models.TextField
+    age = models.IntegerField(default=0)
+    phone = models.TextField(default=None)
+    email = models.TextField(default=None)
+    photo = models.ImageField(default=None)
+    living_place = models.TextField(default=None)
+    skills = models.TextField(default=None)
+    languages= models.TextField(default=None)
+    work_ex = models.TextField(default=None)
     certificate_knewit = models.BooleanField(default=False)
-    Resume = models.FileField(upload_to='resume/')
+    Resume = models.FileField(upload_to='resume/', default=None)
 
 class Claim_work(models.Model):
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, default=None)
     stack = models.CharField(max_length=128)
     frame = models.CharField(max_length=128)
-    desired_salary = models.IntegerField
+    desired_salary = models.IntegerField(default=0)
 
 class Tech(models.Model):
     frame = models.CharField(max_length=128)
@@ -28,10 +28,10 @@ class Tech(models.Model):
 
 class soc_links(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    instagram = models.TextField
-    WPP = models.TextField
-    Github = models.TextField
-    twitter = models.TextField
+    instagram = models.TextField(default=None)
+    WPP = models.TextField(default=None)
+    Github = models.TextField(default=None)
+    twitter = models.TextField(default=None)
 
 class certificates(models.Model):
     file = models.FileField(upload_to='certificates/')
