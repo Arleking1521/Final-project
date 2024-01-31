76d308784b7807a6312813b43fc01925dfc62f78
Arleking <Abdukarim600@gmail.com>
2024-02-01 01:06:06 +0600

    Update Header
    added link to main page

--- front_end/src/Components/Header.jsx
+++ front_end/src/Components/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import logo from '../assets/logo1.png'
+import {useNavigate} from "react-router-dom";
 
 const Header = () => {
+    const navigate = useNavigate();
+    const handleDetailsClick = () => {
+        navigate(`/`);
+    };
     return (
         <div className="myHeader">
             <div className="logo">
@@ -11,7 +16,7 @@ const Header = () => {
                 <span>PEOPLE</span>
             </div>
             <div className="menu">
-                <a href="" className="link">ученики</a>
+                <a href="" className="link" onClick={handleDetailsClick}>ученики</a>
                 <a href="" className="link">Вакансии</a>
                 <a href="https://knewit.kz/" className="link">KnewIT.kz</a>
             </div>
